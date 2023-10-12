2b6afc34c870dc2269b07fd1280d2a12a396c93a
Getasew-worku <143239354+Getasew-worku@users.noreply.github.com>
2023-10-12 17:39:16 -0500

    Test case 1002

---
+++ src/test/java/gitpractice2/Gittest1.java
@@ -0,0 +1,13 @@
+package gitpractice2;
+
+public class Gittest1 {
+	public  void printme1() {
+		System.out.println("git test");
+
+	}
+	public  void printme() {
+		System.out.println("git test");
+
+	}
+
+}
